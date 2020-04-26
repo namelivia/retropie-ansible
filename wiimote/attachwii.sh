c6e4c417239b448a40652abf044291695a301298
José Ignacio Amelivia Santiago <nacho.amelivia@travelperk.com>
2020-04-26 17:23:51 +0200

    Fix wiimote config

--- wiimote/attachwii.sh
+++ wiimote/attachwii.sh
@@ -2,7 +2,7 @@
 sleep 1 # Wait until Bluetooth services are fully initialized 
 hcitool dev | grep hci >/dev/null
 if test $? -eq 0 ; then
-	wminput -d -c /home/pi/mywminput D8:6B:F7:FB:47:F7 > /dev/null 2>&1 & 
+	wminput -d -c ir_ptr D8:6B:F7:FB:47:F7 > /dev/null 2>&1 & 
 else
 	echo "Blue-tooth adapter not present!"
 	exit 1
